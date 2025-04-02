33e2d5304f72f39402e2927000cc2642e19dc750
7onc1 <anton.demirev84@gmail.com>
2025-04-02 15:51:23 +0300

    Fix effect color

--- components/newsletter.js
+++ components/newsletter.js
@@ -102,7 +102,7 @@ export default function Newsletter() {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#129160] to-[#a93136] opacity-40"
+          className="aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#129160] to-[#129160] opacity-50"
         />
       </div>
     </div>
